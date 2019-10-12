a2095fa180c1eb30374173d5391f6010bbd0b7bf
Jalaludeen <connectdeen@gmail.com>
2019-10-12 13:13:58 +0530

    Refactor name

--- src/main/java/penguins/Tailor.java
+++ src/main/java/penguins/Tailor.java
@@ -3,28 +3,28 @@ package penguins;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Arrays.stream;
-
 public class Tailor {
 
 
-    public Bill stitch(List<Materials> matirials) {
+    public Bill stitch(List<Materials> materials) {
         List<Item> items = new ArrayList<>();
-        int bybyMatirial = 0, maleMaterial = 0, femaleMaterial = 0;
-        for (int i = 0; i < matirials.size(); i++) {
-            if (matirials.get(i).equals(Materials.BABY)) {
-                bybyMatirial++;
+
+        int babyMaterial = 0, maleMaterial = 0, femaleMaterial = 0;
+        for (int i = 0; i < materials.size(); i++) {
+            if (materials.get(i).equals(Materials.BABY)) {
+                babyMaterial++;
             }
-            if (matirials.get(i).equals(Materials.MALE)) {
+            if (materials.get(i).equals(Materials.MALE)) {
                 maleMaterial++;
             }
-            if (matirials.get(i).equals(Materials.FEMALE)) {
+            if (materials.get(i).equals(Materials.FEMALE)) {
                 femaleMaterial++;
             }
         }
         items.add(new Item(120, 5, maleMaterial));
         items.add(new Item(145, 7, femaleMaterial));
-        items.add(new Item(70, 3, bybyMatirial));
+        items.add(new Item(70, 3, babyMaterial));
+
         Bill bill = new Bill(items);
         return bill;
     }
